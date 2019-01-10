b2621fc47a5eb32f667fa1e9ee103f6ad55a12c7
Jason Blocklove <jmb1997@comcast.net>
2019-01-09 19:48:14 -0500

    Merge script for calcurse to sync between my server and my local machine

---
+++ packages/cli/calcurse/config/calcurse/script/calcurse_merge.py
@@ -0,0 +1,95 @@
+import sys, os, datetime
+
+def diff_old(current_local, current_remote, file_type):
+    """Diffs current lists for merge with old versions. If anything was deleted it ensures that the deleted item is not in the final list
+
+    :current_local: TODO
+    :current_remote: TODO
+    :returns: TODO
+
+    """
+    with open ('old_versions/' + file_type) as f:
+        old_version = f.readlines()
+
+    for val in old_version:
+        if val not in current_local and val in current_remote:
+            current_remote.remove(val)
+        elif val in current_local and val not in current_remote:
+            current_local.remove(val)
+        else:
+            continue
+
+    total_list = []
+    for val in current_local:
+        if val not in total_list:
+            total_list.append(val)
+    for val in current_remote:
+        if val not in total_list:
+            total_list.append(val)
+
+    return total_list
+
+
+def merge_todo(file_1, file_2):
+    """Merges calcurse todo files
+
+    TODO: Find a way of accounting for deleted todos from either client or server
+        idea: store previous merged versions of both, use a diff to find if lines were deleted
+
+    :file_1: list of local todos
+    :file_2: list of remote todos
+    :returns: total sorted list
+
+    """
+    total_list = diff_old(file_1, file_2, 'todo')
+    total_list.sort()
+    return total_list
+
+def merge_apts(file_1, file_2):
+    """Merges calcurse apts file
+
+    :file_1: list of local apts
+    :file_2: list of remote apts
+    :returns: total sorted list
+
+    """
+    total_list = diff_old(file_1, file_2, 'apts')
+    total_list.sort(key=lambda x: datetime.datetime.strptime(x[:10], '%m/%d/%Y'))
+    return total_list
+
+
+def main():
+    if len(sys.argv) != 2:
+        print ("Usage: calcurse_merge.py <server>")
+        sys.exit(1)
+    else:
+        server = sys.argv[1]
+
+    os.system('mkdir -p tmp && cd tmp && scp ' + server + ':"~/.calcurse/apts ~/.calcurse/todo" .')
+
+    with open('tmp/apts') as f:
+        remote_apts = f.readlines()
+    with open('tmp/todo') as f:
+        remote_todo = f.readlines()
+    with open('/home/jason/.calcurse/apts') as f:
+        local_apts = f.readlines()
+    with open('/home/jason/.calcurse/todo') as f:
+        local_todo = f.readlines()
+
+    todo_list = merge_todo(local_todo, remote_todo)
+    apts_list = merge_apts(local_apts, remote_apts)
+
+    with open('tmp/apts', 'w') as f:
+        for item in apts_list:
+            f.write(item)
+
+    with open('tmp/todo', 'w') as f:
+        for item in todo_list:
+            f.write(item)
+
+
+    os.system('cd tmp && cp apts todo ../old_versions && scp apts todo ' + server + ':~/.calcurse && cp apts todo ~/.calcurse && cd .. && rm -rf tmp')
+
+
+if __name__ == "__main__":
+    main()
