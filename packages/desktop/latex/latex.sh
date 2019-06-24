fc151e6c1b466fafee008990f25d974007a9f106
Jason Blocklove <jmb1997@comcast.net>
2019-06-24 16:11:16 -0400

    changed pip to pip3. Can't test all changes on my system, so some may have issues. Will try to check later

--- packages/desktop/latex/latex.sh
+++ packages/desktop/latex/latex.sh
@@ -13,7 +13,7 @@ ln -sfv "${PACKAGE_INSTALL}/config/zathurarc" ${HOME}/.config/zathura/
 ln -sfv "${PACKAGE_INSTALL}/config/tex.snippets" ${HOME}/.config/nvim/UltiSnips/tex.snippets
 
 # Vimtex needs --remote in vim, so this puts it back for neovim
-pip install --user neovim-remote
+pip3 install --user neovim-remote
 
 # Reinstall plugins for vimtex
 nvim +PluginInstall +qall
