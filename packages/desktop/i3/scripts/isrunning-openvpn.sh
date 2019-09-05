94863e719bb8ac1f5c96e3328eba9325966922a1
Jason Blocklove <jmb1997@comcast.net>
2019-09-05 16:11:38 -0400

    Updated i3 configs and included nord script support

--- packages/desktop/i3/scripts/isrunning-openvpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-OPENVPN_CMD=$(pgrep openvpn | xargs --no-run-if-empty ps -o cmd=)
-if [ "$OPENVPN_CMD" ]; then
-	SPLIT=' ' read -ra PROCESS <<< "$OPENVPN_CMD"
-    echo -e "  ${PROCESS[1]}"
-else
-    echo -e " Not Connected"
-fi
---
+++ packages/desktop/i3/scripts/nord_connection.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import subprocess
+
+nord_status = subprocess.run(['nordvpn', 'status'], stdout=subprocess.PIPE)
+nord_status_list = nord_status.stdout.decode('utf-8').splitlines()
+
+while nord_status_list[0].split(': ')[0] != "Status":
+    del nord_status_list[0]
+    pass
+status_connected = nord_status_list[0].split(': ')[1]
+if status_connected == "Connected":
+    status_country = nord_status_list[2].split(': ')[1]
+    status_city = nord_status_list[3].split(': ')[1]
+    print(" | " + status_country + " - " + status_city)
+    pass
+else:
+    print(" | Not Connected")
+    pass
