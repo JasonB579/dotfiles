d5dc0df602fd98d1f7e6d11ddabe58b2b2a6f608
Jason Blocklove <jmb1997@comcast.net>
2019-09-05 16:03:53 -0400

    Updated neomutt configs

--- packages/cli/neomutt/neomutt.sh
+++ packages/cli/neomutt/neomutt.sh
@@ -18,13 +18,17 @@ fi
 if [ -r "${HOME}/.offlineimaprc" ]; then
         mv ${HOME}/.msmtprc ${HOME}/.dotfiles-bak/
 fi
-if [ -r "${HOME}/.config/neomutt" ]; then
+if [ -r "${HOME}/.config/mutt" ]; then
         mv ${HOME}/.config/neomutt ${HOME}/.dotfiles-bak/
 fi
 
 
 # mutt-wizard install
-git clone https://github.com/JasonB579/mutt-wizard.git ${HOME}/.config/neomutt
+git clone https://github.com/LukeSmithxyz/mutt-wizard.git ${HOME}/.config/mutt
+
+# my configs
+ln -sfv "${PACKAGE_INSTALL}/config/mailcap" "${HOME}/.config/mutt/mailcap"
+ln -sfv "${PACKAGE_INSTALL}/config/muttrc" "${HOME}/.config/mutt/muttrc"
 
 # mail snippets for vim
 ln -sfv "${PACKAGE_INSTALL}/config/mail.snippets" "${HOME}/.config/nvim/UltiSnips/mail.snippets"
